5080dcf9679c2baa823c093cfe9210e5d5430e53
Artem <darknessis91-69@mail.ru>
2020-06-29 19:08:44 +0300

    MainMenu update

---
+++ src/MainMenu.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainMenu extends JFrame {
+
+    public String name;
+    private JTextField username;
+    private JButton serverButton;
+    private JButton clientButton;
+    private Server server;
+    private Client client;
+
+    public MainMenu() {
+
+        super("Snow Owl");
+        username = new JTextField();
+        username.setEditable(true);
+        setSize(300, 100);
+        add(username, BorderLayout.NORTH);
+        username.addActionListener(
+                new ActionListener() {
+                    public void actionPerformed(ActionEvent event) {
+                        name = event.getActionCommand();
+                        System.out.println(name);
+                        username.setEditable(false);
+                    }
+                }
+        );
+
+        serverButton = new JButton("Become a host");
+        add(serverButton, BorderLayout.CENTER);
+        serverButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                server = new Server();
+                server.startRunning();
+            }
+        });
+
+        clientButton = new JButton("Connect to host");
+        add(clientButton, BorderLayout.SOUTH);
+        clientButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                client = new Client("127.0.0.1");
+                client.startRunning();
+            }
+        });
+
+    }
+}
---
+++ src/Mastermind.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Mastermind {
+
+    public static void main(String[] args) {
+
+        MainMenu testMenu = new MainMenu();
+        testMenu.setVisible(true);
+        testMenu.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
